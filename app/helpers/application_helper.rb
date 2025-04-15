1d110639b43947c7ac47c5d038c147538117f3ae
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-04-15 12:46:40 +0200

    Fix details in home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def nav_link(name, path, class_name: "")
-    active_class = current_page?(path) ? "text-indigo-600 font-bold" : "text-gray-700 hover:text-indigo-400"
+    active_class = current_page?(path) ? "text-primary font-bold" : "text-secondary-700 hover:text-indigo-400"
     full_class = [ class_name, active_class ].join(" ").strip
     link_to name, path, class: full_class
   end
