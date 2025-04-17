d907658990885b10e9be67066b429b1a53af292b
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-04-17 19:53:29 +0200

    Add postmark for email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { api_token: ENV["POSTMARK_API_TOKEN"] }
+
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
 
