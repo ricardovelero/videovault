8a6881d179dfb4bb3e9d4f71148bf07bf0fc618b
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-04-17 21:57:55 +0200

    Add missing default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  #
+  Rails.application.routes.default_url_options[:host] = ENV["APP_HOST"]
 end
