4e275c578d5764b0fb0490eefa0dbf99909af3d2
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-04-21 13:16:08 +0200

    Not testing anymore

--- app/jobs/upload_to_cloudinary_job.rb
+++ app/jobs/upload_to_cloudinary_job.rb
@@ -4,10 +4,10 @@ class UploadToCloudinaryJob < ApplicationJob
   def perform(video_id, file_path)
     Rails.logger.info "[UploadToCloudinaryJob] Iniciando subida para Video ##{video_id} desde #{file_path}"
     video = Video.find(video_id)
-    # uploaded = Cloudinary::Uploader.upload(file_path, resource_type: :video)
-    # video.update(file_url: uploaded["secure_url"])
-    sleep 5
-    video.update(file_url: "https://res.cloudinary.com/drkagbdbg/video/upload/v1744963833/us859fnzevykbtviohtw.mp4")
+    uploaded = Cloudinary::Uploader.upload(file_path, resource_type: :video)
+    video.update(file_url: uploaded["secure_url"])
+    # sleep 5
+    # video.update(file_url: "https://res.cloudinary.com/drkagbdbg/video/upload/v1744963833/us859fnzevykbtviohtw.mp4")
 
     Rails.logger.info "[UploadToCloudinaryJob] Subida completada. URL: #{video.file_url}"
 
