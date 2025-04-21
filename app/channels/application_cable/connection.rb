72a13cc705adcca722badf97875c89e2b33c2128
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-04-21 12:23:10 +0200

    Fix current_user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      # Aquí depende de tu sistema de auth. Por ejemplo, si usas Devise con cookies:
+      if env["warden"].user.present?
+        env["warden"].user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
